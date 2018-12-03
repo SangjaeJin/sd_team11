2c097a2eab38d1b08ce75c5521356037f521660b
SangjaeJin <38873936+SangjaeJin@users.noreply.github.com>
2018-12-03 15:28:08 +0900

    Update Transfer.java

--- sd-jinsangjae/Transfer.java
+++ sd-jinsangjae/Transfer.java
@@ -1,9 +1,14 @@
 
+import java.time.format.DateTimeFormatter;
+
 public class Transfer extends Payment {
 
-	public Transfer(String id, int bills) {
-		super(id, bills);
+	private String account;
+	public Transfer(String id, int bills, DateTimeFormatter date,String account) {
+		super(id, bills, date);
+		this.account=account;
 		// TODO Auto-generated constructor stub
 	}
+	
 
 }
