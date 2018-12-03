7d02c08fab64cb41ec8ffdd72d8b24bfc2b7bcf9
SangjaeJin <38873936+SangjaeJin@users.noreply.github.com>
2018-12-03 15:27:48 +0900

    Update Card.java

--- sd-jinsangjae/Card.java
+++ sd-jinsangjae/Card.java
@@ -1,9 +1,14 @@
+import java.time.format.DateTimeFormatter;
 
 public class Card extends Payment {
-
-	public Card(String id, int bills) {
-		super(id, bills);
+	
+	private String cardSerial;
+	
+	public Card(String id, int bills, DateTimeFormatter date,String cardSerial) {
+		super(id, bills, date);
+		this.cardSerial=cardSerial;
 		// TODO Auto-generated constructor stub
+		
 	}
 
 }
