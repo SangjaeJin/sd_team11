6bd8c6d4fd53ce233379a5d1fec46cebf3b0f249
asdf99245 <39851220+asdf99245@users.noreply.github.com>
2018-12-09 20:15:14 +0900

    Update Events.java

--- sd-jinsangjae/Events.java
+++ sd-jinsangjae/Events.java
@@ -8,6 +8,11 @@ public class Events {
 		this.events =new ArrayList<>();
 	}
 	public void show() {
+		if(events.size()==0)
+		{
+			System.out.println("이벤트가 없습니다.");
+			return ;
+		}
 		for(int i=0;i<events.size();i++)
 			System.out.println(events.get(i));
 	}
