3c22e136769e7ee5c3b22a78457021847381a377
SangjaeJin <38873936+SangjaeJin@users.noreply.github.com>
2018-12-19 14:13:18 +0900

    Delete Vip.java

--- sd-jinsangjae/Vip.java
+++
@@ -1,9 +0,0 @@
-
-public class Vip extends Customer {
-
-	public Vip(String name, String id, String pwd, String phone) {
-		super(name, id, pwd, phone);
-		// TODO Auto-generated constructor stub
-	}
-
-}
