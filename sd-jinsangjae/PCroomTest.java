6aedda1dc06fe432d31506a92d66886dc373b636
asdf99245 <39851220+asdf99245@users.noreply.github.com>
2018-12-19 13:57:07 +0900

    Update PCroomTest.java

--- sd-jinsangjae/PCroomTest.java
+++ sd-jinsangjae/PCroomTest.java
@@ -13,6 +13,22 @@ public class PCroomTest {
 		assertEquals("computer num test",24,pc.getComNum());
 		assertEquals("owner id test","testOwn",pc.getOwner().getId());
 		assertEquals("owner passwd test","testOwn",pc.getOwner().getPwd());
+		
+		own = new Owner("testOwn2","testOwn2");
+		pc = new PCroom(own,"testPC2","seoul",5,5,25);
+		assertEquals("name test","testPC2",pc.getName());
+		assertEquals("location tets","seoul",pc.getLocation());
+		assertEquals("computer num test",25,pc.getComNum());
+		assertEquals("owner id test","testOwn2",pc.getOwner().getId());
+		assertEquals("owner passwd test","testOwn2",pc.getOwner().getPwd());
+		
+		own = new Owner("testOwn3","testOwn3");
+		pc = new PCroom(own,"testPC3","ulsan",5,6,30);
+		assertEquals("name test","testPC3",pc.getName());
+		assertEquals("location tets","ulsan",pc.getLocation());
+		assertEquals("computer num test",30,pc.getComNum());
+		assertEquals("owner id test","testOwn3",pc.getOwner().getId());
+		assertEquals("owner passwd test","testOwn3",pc.getOwner().getPwd());
 	}
 
 }
